ff2bb64e7a5f3de27fca28b50f86da02d23167fb
pushkal04 <pushkal.mondal@gmail.com>
2022-02-28 20:07:25 +0530

    Remove item functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberofCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const { items } = cartCtx;
+
+  const btnClases = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClases} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon></CartIcon>
       </span>
